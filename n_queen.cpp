5fa625094729ba9e298a50cae2869a43ee3d5cdc
Jaikrishna Binnar <86294906+jaikrushna@users.noreply.github.com>
2023-05-22 00:07:01 +0530

    Create n_queen.cpp

---
+++ n_queen.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isSafe(vector<vector<int>>& board, int row, int col, int N) {
+    // Check if there is a queen in the same row
+    for (int i = 0; i < col; i++) {
+        if (board[row][i] == 1) {
+            return false;
+        }
+    }
+
+    // Check if there is a queen in the upper left diagonal
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+        if (board[i][j] == 1) {
+            return false;
+        }
+    }
+
+    // Check if there is a queen in the lower left diagonal
+    for (int i = row, j = col; i < N && j >= 0; i++, j--) {
+        if (board[i][j] == 1) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool solveNQueensUtil(vector<vector<int>>& board, int col, int N) {
+    if (col == N) {
+        // All queens are placed successfully
+        return true;
+    }
+
+    for (int row = 0; row < N; row++) {
+        if (isSafe(board, row, col, N)) {
+            board[row][col] = 1;  // Place the queen
+
+            if (solveNQueensUtil(board, col + 1, N)) {
+                return true;
+            }
+
+            // Backtrack and remove the queen if it leads to no solution
+            board[row][col] = 0;
+        }
+    }
+
+    return false;
+}
+
+void solveNQueens(int N) {
+    vector<vector<int>> board(N, vector<int>(N, 0));
+
+    if (solveNQueensUtil(board, 0, N)) {
+        // Print the solution
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+    } else {
+        cout << "No solution exists." << endl;
+    }
+}
+
+int main() {
+    int N;
+    cout << "Enter the number of queens (N): ";
+    cin >> N;
+
+    solveNQueens(N);
+
+    return 0;
+}
